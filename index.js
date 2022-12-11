d89879542fb4536f92356ff17fd62768d4818604
chekolovec <chekolovec@gmail.com>
2022-12-11 21:12:59 +0100

    add cors handling to socket

--- index.js
+++ index.js
@@ -31,8 +31,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
-    credentials: true,
+    origin: process.env.ORIGIN,
   },
 });
 
